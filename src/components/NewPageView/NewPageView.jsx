3f3290c1aab76d2d120193cb84cc994611988b72
Ilya Povarov <ilya.povaroff@gmail.com>
2022-02-08 15:18:55 +0300

    newPage input state logic

--- src/components/NewPageView/NewPageView.jsx
+++ src/components/NewPageView/NewPageView.jsx
@@ -1,16 +1,52 @@
 import './NewPageView.css'
 import LinksForm from '../LinksForm/LinksForm'
+import { useState } from 'react/cjs/react.development'
+import { useDispatch } from 'react-redux'
+import { updatePage } from '../../reducers/pagesReducer'
 
 const PageView = props => {
+
+    const dispatch = useDispatch();
+    const [tempPage, setTempPage] = useState({
+        path: '',
+        title: '',
+        text: '',
+        links: []
+    })
+
+    const handleInput = (type) => (event) => { //event.target.value
+        setTempPage({
+            ...tempPage,
+            [type]: event.target.value
+        })
+    }
+
+    const handleLinkUpdate = link => {
+        console.log('link', link)
+        setTempPage({
+            ...tempPage,
+            links: tempPage.links.map(n => n.id === link.id ? link : n)
+        })
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        // dispatch(postPage());
+    }
+
     return (
         <div className="pageView">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>path: </label>
-                <input className='textInput' />
+                <input className='textInput' value={tempPage.path} onChange={handleInput('path')} />
                 <label>title: </label>
-                <input className='textInput' />
+                <input className='textInput' value={tempPage.title} onChange={handleInput('title')} />
                 <label>text: </label>
-                <textarea />
+                <textarea value={tempPage.text} onChange={handleInput('text')} />
+
+                {tempPage.links && tempPage.links.map(link => 
+                       <LinksForm key={link.id} link={link} handleLinkUpdate={handleLinkUpdate}/> 
+                )}
                 <LinksForm />
                 <input type='submit' />
             </form>
